59c8eaa5320b0b302cbd62445cc159acc12a814c
resonatingmind <adityakumarsince2004@kgpian.iitkgp.ac.inclear>
2024-05-15 00:41:38 +0530

    Making some changes

--- Frontend/Course website/src/components/footer.jsx
+++ Frontend/Course website/src/components/footer.jsx
@@ -1,7 +1,14 @@
 function Footer(){
     return(
-        <div className="footer">
-            This is footer
+        <div className="footerContainer">
+            <div className="footer">
+               <div>
+                   
+               </div>
+            </div>
+            <div className="fCredit">
+                Made By <span> Aditya Kumar</span>
+            </div>
         </div>
     )
 };
--- Frontend/Course website/src/components/header.jsx
+++ Frontend/Course website/src/components/header.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 
 function Header(){
+
+  const [isActive, setActive]=useState(0);
+
+  function handleHover(target)
+  {
+    setActive(1);
+  }
+
+  function handleOut(target)
+  {
+    setActive(0);
+  }
+
     return(
         <div className="header">
           <div className="headerLogoContainer">
-           <img className="headerLogo" src="\public\kdagLogo.jpg"></img>
+           <img className="headerLogo" src="/kdagLogo.jpg"></img>
            <div>
               <h2><b>Learn</b><span style={{fontWeight:"100"}}>Bridge</span></h2>
               <p style={{fontSize:"10px"}}>Powered By KDAG</p>
@@ -13,14 +27,15 @@ function Header(){
            <form className="headerElem">
              <input className="headerSearch" type="text" placeholder="Search Courses"/>
              <a href="/search" className="headerSearchIcon"><i class="fa-solid fa-magnifying-glass"></i></a>
-             <div className="headerCate"><i class="fa-solid fa-layer-group"></i>Categories</div>
+             <div onMouseOver={handleHover} onMouseOut={handleOut} className="headerCateDiv"><div className="headerCate"><i class="fa-solid fa-layer-group"></i>Categories</div></div>
            </form>
-           <div className="headerList">
+
+           <div onMouseOver={handleHover} onMouseOut={handleOut} className={isActive ? "showHeaderCateList headerList" : "hideHeaderCateList headerList"}>
              <a href="/" className="headerCategoryEach">Category 1</a>
-             <a href="/" className="headerCategoryEach">Category 1</a>
-             <a href="/" className="headerCategoryEach">Category 1</a>
-             <a href="/" className="headerCategoryEach">Category 1</a>
-             <a href="/" className="headerCategoryEach">Category 1</a>
+             <a href="/" className="headerCategoryEach">Category 2</a>
+             <a href="/" className="headerCategoryEach">Category 3</a>
+             <a href="/" className="headerCategoryEach">Category 4</a>
+             <a href="/" className="headerCategoryEach">Category 5</a>
            </div>
         </div>
     )
